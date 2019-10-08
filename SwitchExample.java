a8965e0b008f3a22d8586120bd8416cf8e4712e4
Sevilla <10488@vtsd.com>
2019-10-08 08:38:23 -0400

    10/8/2019

---
+++ SwitchExample.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class examples{
+  public static void main(String args[]){
+
+//Then we need to make an obejct of the Scanner
+  Scanner input = new Scanner(System.in);
+  System.out.println("Enter a number 1 to 5");
+  int choice = input.nextInt();
+  
+
+
+
+
+
+
+
+  }
+}
--- examples.java
+++ examples.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 
 public class examples{
   public static void main(String args[]){
+
+//Then we need to make an obejct of the Scanner
+  Scanner input = new Scanner(System.in);
+
+//Lastly, we can then use the scanner to take input
+  System.out.println("Please enter amount you sold");
+  int sales = input.nextInt();
+  System.out.println("your sales are " + sales);
+  if(sales < 100000 ){
+System.out.println("You suck your fired  !");
+}else if(sales >= 100000 && sales <= 300000){
+System.out.println("Your are average, good job");
+}
+if(sales > 300000){
+  System.out.println("Great Job, Take my money  ");
+}
 
 
   }

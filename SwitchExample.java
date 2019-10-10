1f95796a9d3da1584cd20dfb792593c871447a3a
Sevilla <10488@vtsd.com>
2019-10-10 08:12:25 -0400

    10/10/19

--- SwitchExample.java
+++ SwitchExample.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
 
-public class examples{
+public class SwitchExample{
   public static void main(String args[]){
 
 //Then we need to make an obejct of the Scanner
   Scanner input = new Scanner(System.in);
-  System.out.println("Enter a number 1 to 5");
+  System.out.println("Pick a verse  1 to 5");
   int choice = input.nextInt();
-  
+  switch(choice){
+    case 1: System.out.println("1 Partriddge in a Pear Tree ");
+    case 2: System.out.println("2 Turtle Doves");
+    case 3: System.out.println("3 French Hens");
+    case 4: System.out.println("4 Golden Rings");
+    case 5: System.out.println("5 DARN IT YOU MESSED UP FOOL ");
+
+  }
 
 
 
 
 
-
-
+//wiibowling.net
   }
 }
